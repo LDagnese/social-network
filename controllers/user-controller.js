6e8aee86a0b819d3915728846cdf717eaec1a6ee
loudagnese <louis.dagnese@gmail.com>
2022-09-05 09:43:54 -0700

    finish user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,7 @@ deleteUser,
 addFriend,
 removeFriend */
 
-const { User, Thought } = require("../models");
+const { User } = require("../models");
 
 const userController = {
   getUsers(req, res) {
@@ -23,6 +23,7 @@ const userController = {
   getUserById({ params }, res) {
     User.findOne({ _id: params.userId })
       .select("-__v")
+      .populate("friends")
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "No user with this id!" });
@@ -41,6 +42,77 @@ const userController = {
         res.json(dbUserData);
       })
       .catch((err) => res.status(500).json(err));
+  },
+  updateUser({ params, body }, res) {
+    User.findByIdAndUpdate(
+      params.userId,
+      {
+        $set: body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findByIdAndDelete(req.params.userId)
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  addFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $addToSet: { friends: req.params.friendId },
+      },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  removeFriend(req, res) {
+    User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 };
 
